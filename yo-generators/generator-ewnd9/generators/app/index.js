1da805d171e925b5794d9f2580df888103b3b168
ewnd9 <ewndnine@gmail.com>
2015-08-02 19:30:42 +0300

    add travis.yml to generator

--- yo-generators/generator-ewnd9/generators/app/index.js
+++ yo-generators/generator-ewnd9/generators/app/index.js
@@ -33,17 +33,21 @@ module.exports = yeoman.generators.Base.extend({
     },
 
     projectfiles: function () {
-      this.fs.copy(
-        this.templatePath('editorconfig'),
-        this.destinationPath('.editorconfig')
-      );
-      this.fs.copy(
-        this.templatePath('jshintrc'),
-        this.destinationPath('.jshintrc')
-      );
+      // this.fs.copy(
+      //   this.templatePath('editorconfig'),
+      //   this.destinationPath('.editorconfig')
+      // );
+      // this.fs.copy(
+      //   this.templatePath('jshintrc'),
+      //   this.destinationPath('.jshintrc')
+      // );
       this.fs.copy(
         this.templatePath('gitignore'),
         this.destinationPath('.gitignore')
+      );
+      this.fs.copy(
+        this.templatePath('travis.yml'),
+        this.destinationPath('.travis.yml')
       );
     }
   },
