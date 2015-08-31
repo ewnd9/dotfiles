d8df814ea8f463fe7a85031fe02a4c97a3ebb7a5
ewnd9 <ewndnine@gmail.com>
2015-09-01 00:09:50 +0300

    add image loader to webpack template

--- yo-generators/generator-ewnd9/generators/app/templates/webpack-dev-server/webpack.config.js
+++ yo-generators/generator-ewnd9/generators/app/templates/webpack-dev-server/webpack.config.js
@@ -47,8 +47,19 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loader: 'style!css!sass?outputStyle=expanded' 
+        loader: 'style!css!sass?outputStyle=expanded'
       },
+      {
+        test: /\.css$/,
+        loader: "style-loader!css-loader"
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+          'file?hash=sha512&digest=hex&name=[hash].[ext]',
+          'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
+        ]
+      }
     ]
   },
   plugins: plugins,
