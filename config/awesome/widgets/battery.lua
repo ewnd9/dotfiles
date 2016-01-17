04d1b307821bfec198ebaa6a2426cb9c6c60c35c
ewnd9 <ewndnine@gmail.com>
2016-01-17 19:24:55 +0800

    highlight battery level less than 20% instead of less than 30%

--- config/awesome/widgets/battery.lua
+++ config/awesome/widgets/battery.lua
@@ -6,7 +6,7 @@ vicious.register(batwidget, vicious.widgets.bat,
     if args[2] == 0 then return ""
     else
       baticon.image = image(beautiful.widget_bat)
-      if args[2] < 30 then
+      if args[2] < 20 then
         return "<span color='red'>".. args[2] .. "%</span>"
       else
         return "<span color='white'>".. args[2] .. "%</span>"
