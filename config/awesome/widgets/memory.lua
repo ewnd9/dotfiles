775d7f996d2a1b78c0ff77335efcffc3a08a54c9
ewnd9 <ewndnine@gmail.com>
2018-02-22 19:36:34 +0800

    chore(awesome): remove old config

--- config/awesome/widgets/memory.lua
+++
@@ -1,5 +0,0 @@
-local wibox = require("wibox")
-local vicious = require("vicious")
-
-memtext = wibox.widget.textbox()
-vicious.register(memtext, vicious.widgets.mem, "$2MB/$3MB", 13)
--- config/awesome/widgets/spacer.lua
+++
@@ -1,4 +0,0 @@
-local wibox = require("wibox")
-
-spacer = wibox.widget.textbox()
-spacer:set_text(" ")
