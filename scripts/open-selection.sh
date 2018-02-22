9a6f785407f6f50d1c29bf86adad39e53fe4542a
ewnd9 <ewndnine@gmail.com>
2018-02-22 19:39:12 +0800

    chore(*): clean up

--- scripts/open-selection.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-QUERY=$(xsel -o)
-
-notify-send -t 1000 opening
-$HOME/.npm-packages/bin/cached-npm-repo "$QUERY"
-
-[ $? = 0 ] && echo ok || notify-send -t 1000 "\"$QUERY\" is not found"
