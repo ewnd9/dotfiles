cd71ca3ab442adb70a586c220f2c5bc0feb39c5f
ewnd9 <ewndnine@gmail.com>
2018-04-19 11:18:04 +0800

    feat(tmux): add tmux/focus.sh

---
+++ scripts/tmux/focus.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e
+
+export LC_NUMERIC="en_US.UTF-8"
+
+# https://superuser.com/a/433702
+CURRENT_WINDOW=`tmux list-window | tr '\t' ' ' | sed -n -e '/(active)/s/^[^:]*: *\([^ ]*\) .*/\1/gp'`;
+CURRENT_PANE=`tmux list-panes | sed -n -e '/(active)/s/^\([^:]*\):.*/\1/gp'`;
+PANE_ID="$CURRENT_WINDOW.$CURRENT_PANE"
+
+WINDOW_HEIGHT=$(tmux display -p '#{window_height}')
+PANE_HEIGHT=$(tmux display -p '#{pane_height}')
+
+EIGHTY_PERCENT_=$(echo "scale=0; $WINDOW_HEIGHT * 0.8" | bc)
+EIGHTY_PERCENT=$(printf "%.0f\n" $EIGHTY_PERCENT_)
+
+BACKUP_FILE="/tmp/tmux-focus"
+
+if [[ "$PANE_HEIGHT" -gt "$EIGHTY_PERCENT" ]]; then
+  for var in BACKUP_PANE BACKUP_LAYOUT; do
+    IFS= read -r "$var" || break
+  done < $BACKUP_FILE
+
+  if [[ "$PANE_ID" == "$BACKUP_PANE" ]]; then
+    tmux select-layout $BACKUP_LAYOUT
+  fi
+else
+  TMUX_LAYOUT=$(tmux display-message -p "#{window_layout}")
+  printf "$PANE_ID\n$TMUX_LAYOUT" > $BACKUP_FILE
+  tmux resize-pane -y 100
+fi
+
