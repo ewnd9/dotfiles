2050fdc85e6ff641e433c22e81e2d7ce4f1d4ecb
ewnd9 <ewndnine@gmail.com>
2018-04-19 22:36:04 +0800

    feat(hyper): init
    A few things are needed before switching as a main terminal
    (see @TODO in config/.hyper.js)
    
    It's renders faster than gnome-terminal though

---
+++ config/.hyper.js
@@ -0,0 +1,68 @@
+'use strict';
+
+// Future versions of Hyper may add additional config options,
+// which will not automatically be merged into this file.
+// See https://hyper.is#cfg for all currently supported options.
+
+/**
+  @TODO:
+    - alt keys
+    - start with tmux
+    - cursor change on vim mode switch
+    - ligatures (see below)
+ */
+
+module.exports = {
+  config: {
+    updateChannel: 'stable',
+    fontSize: 14,
+    // Ligatures are not supported yet https://github.com/xtermjs/xterm.js/issues/958
+    fontFamily: '"Fira Code", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontWeight: 'normal',
+    fontWeightBold: 'bold',
+    cursorColor: 'rgba(248,28,229,0.8)',
+    cursorAccentColor: '#000',
+    // `'BEAM'` for |, `'UNDERLINE'` for _, `'BLOCK'` for █
+    cursorShape: 'BLOCK',
+    cursorBlink: false,
+    foregroundColor: '#fff',
+    backgroundColor: '#000',
+    selectionColor: 'rgba(248,28,229,0.3)',
+    borderColor: '#333',
+    css: '',
+    termCSS: '',
+    showHamburgerMenu: '',
+    showWindowControls: false,
+    padding: '12px 14px',
+    colors: {
+      black: '#000000',
+      red: '#C51E14',
+      green: '#1DC121',
+      yellow: '#C7C329',
+      blue: '#0A2FC4',
+      magenta: '#C839C5',
+      cyan: '#20C5C6',
+      white: '#C7C7C7',
+      lightBlack: '#686868',
+      lightRed: '#FD6F6B',
+      lightGreen: '#67F86F',
+      lightYellow: '#FFFA72',
+      lightBlue: '#6A76FB',
+      lightMagenta: '#FD7CFC',
+      lightCyan: '#68FDFE',
+      lightWhite: '#FFFFFF',
+    },
+    shell: '',
+    shellArgs: ['--login'],
+    env: {},
+    bell: false,
+    copyOnSelect: true,
+    defaultSSHApp: true,
+  },
+  plugins: ["hyper-one-dark"],
+  localPlugins: [],
+  keymaps: {
+    // Example
+    // 'window:devtools': 'cmd+alt+o',
+  },
+};
