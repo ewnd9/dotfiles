00b96bc92506159c3c4ca1355c8a6a1664a78a11
ewnd9 <ewndnine@gmail.com>
2018-05-19 16:12:51 +0300

    feat(belt): add flash-drive

---
+++ packages/belt-ewnd9/commands/flash-drive.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const execa = require('belt-tools/modules/execa');
+const prompts = require('prompts');
+
+module.exports = {
+  run
+};
+
+async function run() {
+  const stdout = await execa.stdout('sudo', ['fdisk', '-l'], { stdio: [0, 'pipe', 'pipe'] });
+  const lines = stdout.split('\n');
+  const devices = lines
+    .filter(_ => _.startsWith('/dev/sd'))
+    .map(line => {
+      const parts = line.split(/\s+/);
+
+      const name = parts[0];
+      const size = parts[4];
+
+      return { name, size };
+    });
+
+  const mounts = await getMounts();
+  const dir = '/media/flashdrive';
+
+  const { value } = await prompts({
+    type: 'select',
+    name: 'value',
+    message: 'Pick a device',
+    choices: devices.map(({ name, size }, i) => {
+      const parts = [`${name} (${size})`];
+
+      if (mounts[name] === dir) {
+        parts.push('[already mounted]');
+      }
+
+      return {
+        title: parts.join(' '),
+        value: i
+      };
+    }),
+    initial: devices.length - 1
+  });
+
+  const device = devices[value];
+
+  if (!device) {
+    return;
+  }
+
+  await execa('sudo', ['mkdir', '-p', dir], { stdio: 'inherit' });
+  await execa('sudo', ['mount', device.name, dir], { stdio: 'inherit' });
+  await execa('ls', ['-al'], { stdio: 'inherit' });
+}
+
+async function getMounts() {
+  const stdout = await execa.stdout('cat', ['/proc/mounts']);
+  return stdout
+    .split('\n')
+    .reduce((acc, line) => {
+      const [src, dest] = line.split(' ');
+
+      if (src.startsWith('/dev/sd')) {
+        acc[src] = dest;
+      }
+
+      return acc;
+    }, {})
+}
+
