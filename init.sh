9bc2fe33448407928171b84c2ec1b57e47ab6fe8
ewnd9 <ewndnine@gmail.com>
2021-03-29 09:48:04 +0300

    update init.sh

--- init.sh
+++ init.sh
@@ -2,56 +2,52 @@
 
 set -ex
 
-cd ~
-# rm -rf Desktop Music Public Videos Documents examples.desktop Templates
-
-# sudo apt-get update && sudo apt-get upgrade
-# sudo apt-get install -y git vim-gtk
-
-# brew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-
-# node
-curl https://get.volta.sh | bash
+bash vendor/volta.sh
 volta install node@14
 volta install yarn
 npm config set prefix $HOME/.npm-packages
 
-## yarn
-curl -o- -L https://yarnpkg.com/install.sh | bash
+if [[ "$OSTYPE" == *darwin* ]]; then
+  bash vendor/brew.sh
+else
+  cd $HOME
+  rm -rf Desktop Music Public Videos Documents examples.desktop Templates
+  cd $HOME/dotfiles
 
-## brew
-brew install kubectl
+  sudo apt-get update && sudo apt-get upgrade
+  sudo apt-get install -y -qq git vim-gtk
 
-# # git clone https://github.com/ewnd9/dotfiles.git
-# cd ~/dotfiles
-
-# sudo apt-add-repository ppa:neovim-ppa/stable
-# sudo apt-key adv --fetch-keys http://dl.yarnpkg.com/debian/pubkey.gpg
-# echo "deb http://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-# sudo apt-get update -qq
-# sudo apt-get install -y -qq yarn neovim
+  sudo apt-add-repository ppa:neovim-ppa/stable
+  sudo apt-get update -qq
+  sudo apt-get install -y -qq neovim
+fi
 
 yarn install
 yarn global add @belt/cli
-~/.config/yarn/global/node_modules/.bin/belt link packages/belt-ewnd9
+$HOME/.config/yarn/global/node_modules/.bin/belt link packages/belt-ewnd9
 
-~/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup apt
-~/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup symlinks
-~/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup submodules
-~/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup npm
-~/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup code
+$HOME/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup apt
+$HOME/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup symlinks
+$HOME/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup submodules
+$HOME/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup npm
+$HOME/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup code
 
-# # firecode
-# mkdir -p  ~/.local/share/fonts
-# for type in Bold Light Medium Regular Retina; do
-#   wget -O ~/.local/share/fonts/FiraCode-${type}.ttf \
-#     "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
-# done
-# fc-cache -f
+if [[ "$OSTYPE" == *darwin* ]]; then
+  $HOME/.config/yarn/global/node_modules/.bin/belt ewnd9:provision --setup brew
+else
+  # firecode
+  mkdir -p  ~/.local/share/fonts
 
-# # change default shell to zsh
-# chsh -s /bin/zsh
+  for type in Bold Light Medium Regular Retina; do
+    wget -O ~/.local/share/fonts/FiraCode-${type}.ttf \
+      "https://github.com/tonsky/FiraCode/blob/master/distr/ttf/FiraCode-${type}.ttf?raw=true";
+  done
+
+  fc-cache -f
+
+  # change default shell to zsh
+  chsh -s /bin/zsh
+fi
 
 # # install gnome-terminal theme, needed to applied after install (`Edit -> Profiles -> Select as default`)
 # ./scripts/gnome-terminal-themes/one-dark.sh
