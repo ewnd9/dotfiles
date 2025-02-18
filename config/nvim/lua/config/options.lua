1ed0ab90a0e664c932b7b8128fb8a588833894cd
ewnd9 <ewndnine@gmail.com>
2025-02-18 10:49:21 +0300

    add LazyVim

---
+++ config/nvim/lua/config/options.lua
@@ -0,0 +1,7 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Defaults https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+
+vim.g.autoformat = false
+vim.opt.wrap = true
+vim.g.lazyvim_prettier_needs_config = true
+
