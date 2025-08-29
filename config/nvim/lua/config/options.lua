974da9e3bfe8c160b5164ad322875ca0ec90d3d5
ewnd9 <ewndnine@gmail.com>
2025-08-29 10:54:45 +0300

    replace neovim configs with LazyVim clone

--- config/nvim/lua/config/options.lua
+++
@@ -1,7 +0,0 @@
--- Options are automatically loaded before lazy.nvim startup
--- Defaults https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
-
-vim.g.autoformat = false
-vim.opt.wrap = true
-vim.g.lazyvim_prettier_needs_config = true
-
